530758702f0bb9317c5b02b216c87874299f86b4
Himanshu Vinchhi <himanshu_vinchhi@outlook.com>
2024-08-06 00:12:27 +0530

    updated with rest service

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -16,4 +16,9 @@ public class GreetingController {
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+
+	@GetMapping("/greeting2")
+	public Greeting greeting2(@RequestParam(value = "name", defaultValue = "World2") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
 }
