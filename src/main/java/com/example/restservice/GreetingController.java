beaa83aee1bcce54781f4feb2cee071d4732f294
Himanshu Vinchhi <himanshu_vinchhi@outlook.com>
2024-08-06 11:59:06 +0530

    CHANGES

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -16,32 +16,4 @@ public class GreetingController {
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
-
-	@GetMapping("/greeting2")
-	public Greeting greeting2(@RequestParam(value = "name", defaultValue = "World2") String name) {
-		// Unused variable
-		int unusedVariable = 42;
-
-		// Potential null pointer exception
-		String nullString = null;
-		if (nullString.equals("test")) {
-			System.out.println("This will throw a NullPointerException");
-		}
-
-		// Duplicated string literal
-		String duplicateString = "duplicate";
-		if ("duplicate".equals(duplicateString)) {
-			System.out.println("This is a duplicated string literal");
-		}
-
-		// Dead code
-		unusedMethod();
-
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-	}
-
-	private void unusedMethod() {
-		System.out.println("This method is never used");
-	}
-
 }
